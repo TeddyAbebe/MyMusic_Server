bc79504c6c2e5973feac50060558960b57d079de
Tewodros Abebe <teddyab729@gmail.com>
2023-03-14 17:36:49 +0300

    Update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const SongList = require("./models/Song");
 
+
 // Create express app
 const app = express();
 
@@ -47,23 +48,28 @@ app.get("/read", async (req, res) => {
   }
 });
 
-app.put("/update/:id", async (req, res) => {
-  const id = req.params.id;
-  const newTitle = req.body.title;
-  const newArtist = req.body.artist;
-  const newAlbum = req.body.album;
-  const newGenre = req.body.genre;
+app.put("/update", async (req, res) => {
+  const id = req.body.id;
+  const newTitle = req.body.newTitle;
+  const newArtist = req.body.newArtist;
+  const newAlbum = req.body.newAlbum;
+  const newGenre = req.body.newGenre;
 
   try {
-    const updatedSong = await SongList.findByIdAndUpdate(
-      id,
-      { title: newTitle, artist: newArtist, album: newAlbum, genre: newGenre },
-      { new: true }
-    );
-    res.send(updatedSong);
+    try {
+      const updatedSong = await SongList.findById(id);
+      updatedSong.title = newTitle;
+      updatedSong.artist = newArtist;
+      updatedSong.album = newAlbum;
+      updatedSong.genre = newGenre;
+      await updatedSong.save();
+      res.send("updated");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    }
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
+    console.log(err);
   }
 });
 
