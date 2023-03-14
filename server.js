f734223ee5b0107f87d5c20642b3709c8f8a4cfb
Tewodros Abebe <teddyab729@gmail.com>
2023-03-14 16:07:52 +0300

    Update

--- server.js
+++ server.js
@@ -80,86 +80,86 @@ app.delete("/delete/:id", async (req, res) => {
   res.send("deleted");
 });
 
-app.get("/statistics", async (req, res) => {
-  const stats = await SongList.aggregate([
-    // Total # of songs, artists, albums, genres
-    {
-      $group: {
-        _id: null,
-        totalSongs: { $sum: 1 },
-        totalArtists: { $addToSet: "$artist" },
-        totalAlbums: { $addToSet: "$album" },
-        totalGenres: { $addToSet: "$genre" },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        totalSongs: 1,
-        totalArtists: { $size: "$totalArtists" },
-        totalAlbums: { $size: "$totalAlbums" },
-        totalGenres: { $size: "$totalGenres" },
-      },
-    },
-  ]);
+// app.get("/statistics", async (req, res) => {
+//   const stats = await SongList.aggregate([
+//     // Total # of songs, artists, albums, genres
+//     {
+//       $group: {
+//         _id: null,
+//         totalSongs: { $sum: 1 },
+//         totalArtists: { $addToSet: "$artist" },
+//         totalAlbums: { $addToSet: "$album" },
+//         totalGenres: { $addToSet: "$genre" },
+//       },
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         totalSongs: 1,
+//         totalArtists: { $size: "$totalArtists" },
+//         totalAlbums: { $size: "$totalAlbums" },
+//         totalGenres: { $size: "$totalGenres" },
+//       },
+//     },
+//   ]);
 
-  const genreStats = await SongList.aggregate([
-    // # of songs in every genre
-    {
-      $group: {
-        _id: "$genre",
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        genre: "$_id",
-        count: 1,
-      },
-    },
-  ]);
+//   const genreStats = await SongList.aggregate([
+//     // # of songs in every genre
+//     {
+//       $group: {
+//         _id: "$genre",
+//         count: { $sum: 1 },
+//       },
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         genre: "$_id",
+//         count: 1,
+//       },
+//     },
+//   ]);
 
-  const artistStats = await SongList.aggregate([
-    // # of songs & albums each artist has
-    {
-      $group: {
-        _id: "$artist",
-        songs: { $addToSet: "$title" },
-        albums: { $addToSet: "$album" },
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        artist: "$_id",
-        songs: { $size: "$songs" },
-        albums: { $size: "$albums" },
-        count: 1,
-      },
-    },
-  ]);
+//   const artistStats = await SongList.aggregate([
+//     // # of songs & albums each artist has
+//     {
+//       $group: {
+//         _id: "$artist",
+//         songs: { $addToSet: "$title" },
+//         albums: { $addToSet: "$album" },
+//         count: { $sum: 1 },
+//       },
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         artist: "$_id",
+//         songs: { $size: "$songs" },
+//         albums: { $size: "$albums" },
+//         count: 1,
+//       },
+//     },
+//   ]);
 
-  const albumStats = await SongList.aggregate([
-    // # of songs in each album
-    {
-      $group: {
-        _id: "$album",
-        count: { $sum: 1 },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        album: "$_id",
-        count: 1,
-      },
-    },
-  ]);
+//   const albumStats = await SongList.aggregate([
+//     // # of songs in each album
+//     {
+//       $group: {
+//         _id: "$album",
+//         count: { $sum: 1 },
+//       },
+//     },
+//     {
+//       $project: {
+//         _id: 0,
+//         album: "$_id",
+//         count: 1,
+//       },
+//     },
+//   ]);
 
-  res.json({ stats, genreStats, artistStats, albumStats });
-});
+//   res.json({ stats, genreStats, artistStats, albumStats });
+// });
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001...");
