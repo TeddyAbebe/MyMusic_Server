eabdae19ea82296cc16cd63276bd90b3b53f29f4
Tewodros Abebe <teddyab729@gmail.com>
2023-03-15 09:21:56 +0300

    Message

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 
 const SongList = require("./models/Song");
 
-
 // Create express app
 const app = express();
 
@@ -48,28 +47,35 @@ app.get("/read", async (req, res) => {
   }
 });
 
-app.put("/update", async (req, res) => {
-  const id = req.body.id;
+app.put("/update/:id", async (req, res) => {
+  const id = req.params.id;
   const newTitle = req.body.newTitle;
   const newArtist = req.body.newArtist;
   const newAlbum = req.body.newAlbum;
   const newGenre = req.body.newGenre;
 
+  console.log("Updating song with ID:", id);
+  console.log("New title:", newTitle);
+  console.log("New artist:", newArtist);
+  console.log("New album:", newAlbum);
+  console.log("New genre:", newGenre);
+
   try {
-    try {
-      const updatedSong = await SongList.findById(id);
-      updatedSong.title = newTitle;
-      updatedSong.artist = newArtist;
-      updatedSong.album = newAlbum;
-      updatedSong.genre = newGenre;
-      await updatedSong.save();
-      res.send("updated");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    }
+    const updatedSong = await SongList.findById(id);
+    console.log("Current song data:", updatedSong);
+
+    updatedSong.title = newTitle;
+    updatedSong.artist = newArtist;
+    updatedSong.album = newAlbum;
+    updatedSong.genre = newGenre;
+
+    console.log("Updated song data:", updatedSong);
+
+    await updatedSong.save();
+    res.send("updated");
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
